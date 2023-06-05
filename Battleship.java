f5c5375f5d723d8cf62965d845fe480c32ed0e73
AlexLi132 <130993289+AlexLi132@users.noreply.github.com>
2023-06-05 08:32:53 -0400

    Added battleship

---
+++ Battleship.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Battleship
+ * 
+ * @author Alex
+ * @version June 5
+ */
+public class Battleship extends Actor
+{
+    /**
+     * Act - do whatever the Battleship wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}
